b2a8efff9f306a23e80403daec6e3fae582f30e1
RakkeshIT <rakkeshit@gmail.com>
2026-06-21 13:29:25 +0530

    Remove "process.exit(1);" for prod

--- config/db.ts
+++ config/db.ts
@@ -7,10 +7,10 @@ dotenv.config();
 
 export const connect = async (): Promise<void> => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI as string);
+        const conn = await mongoose.connect(process.env.MONGO_URI!);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${(error as Error).message}`);
-        process.exit(1);
+        // process.exit(1);
     }
 }
